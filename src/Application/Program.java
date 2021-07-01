c53a005740fb8deeb00c55b567bce461dc81b55e
rafaelssilva306 <rafa.306@hotmail.com>
2021-07-01 00:18:29 -0300

    Update implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -38,10 +38,16 @@ public class Program {
 			
 		
 		}
-			System.out.println("\n === TEST 3: seller findAll =====");
+			System.out.println("\n === TEST 4: new Id =====");
 			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(0), 4000.0, department);
 			sellerDao.insert(newSeller);
 			System.out.println("Inserted! New id = " + newSeller.getId());
+			
+			System.out.println("\n === TEST 5: seller update =====");
+			seller  = sellerDao.findById(1);
+			seller.setName("Marta Waine");
+			sellerDao.update(seller);
+			System.out.println("Updated completed");
 	}
 
 }
